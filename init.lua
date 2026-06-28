5044450192137e03484c442a454872a7bf1dbac3
Thuan Ngo <thuan.ngolegia@gmail.com>
2026-06-28 13:53:09 +0200

    simplify autocommand treesitter

--- init.lua
+++ init.lua
@@ -91,23 +91,7 @@ vim.keymap.set('n', '<leader>f', function()
 )
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = "*",
-  callback = function(args)
-    local buf = args.buf
-    local ft = vim.bo[buf].filetype
-
-    local lang = vim.treesitter.language.get_lang(ft)
-    if not lang then
-      return
-    end
-
-    local ok_add = pcall(vim.treesitter.language.add, lang)
-    if not ok_add then
-      return
-    end
-
-    pcall(vim.treesitter.start, buf, lang)
-  end,
+    callback = function() pcall(vim.treesitter.start) end,
 })
 
 require("gitsigns").setup({
